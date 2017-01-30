46a6d54abcf629b18d5696cf594ef11447778077
klaudia borowska <canimi18@gmial.com>
2017-01-30 16:56:17 +0100

    done with little late

--- geometry.py
+++ geometry.py
@@ -1,3 +1,5 @@
+import math
+import ui
 class Shape:
     """
     This is a abstract class representing geometrical shape.
@@ -5,17 +7,15 @@ class Shape:
 
     def __init__(self):
         """
-        Constructs Shape object
-
-        Raises:
-            ValueError: If any of the parameters is below 0.
-        """
+         Constructs Shape object
+         Raises:
+             ValueError: If any of the parameters is below 0.
+         """
         pass
 
     def get_area(self):
         """
         Calculates shape's area.
-
         Returns:
             float: area of the shape
         """
@@ -24,7 +24,6 @@ class Shape:
     def get_perimeter(self):
         """
         Calculates shape's perimeter.
-
         Returns:
             float: perimeter of the shape
         """
@@ -33,7 +32,6 @@ class Shape:
     def __str__(self):
         """
         Returns information about the shape as string.
-
         Returns:
             str: information bout shape
         """
@@ -43,7 +41,6 @@ class Shape:
     def get_area_formula(cls):
         """
         Returns formula for the area of the shape as a string.
-
         Returns:
             str: area formula
         """
@@ -53,7 +50,6 @@ class Shape:
     def get_perimeter_formula(cls):
         """
         Returns formula for the perimeter of the shape as a string.
-
         Returns:
             str: perimeter formula
         """
@@ -61,28 +57,195 @@ class Shape:
 
 
 class Circle(Shape):
-    pass
+
+    def __init__(self, r):
+        self.r = r
+        if r < 0 :
+            raise ValueError("Circle cannot have negative radius")
+
+
+    def get_area(self):
+        return math.pi * (self.r ** 2)
+
+
+    def get_perimeter(self):
+        return 2 * math.pi * self.r
+
+
+    def __str__(self):
+        return "Circle, r = " + str(self.r)
+
+
+    @classmethod
+    def get_area_formula(cls):
+        return "π×r^2"
+
+
+    @classmethod
+    def get_perimeter_formula(cls):
+        return "2×π×r"
 
 
 class Triangle(Shape):
-    pass
+
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        if a < 0 or b < 0 or c < 0:
+            raise ValueError("Values of the parameters can't be below 0")
+
+    def get_area(self):
+        s = (self.a + self.b + self.c) / 2
+        return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
+
+    def get_perimeter(self):
+        return self.a + self.b + self.c
+
+    def __str__(self):
+        return "Triangle, a=" + str(self.a) + ",b=" + str(self.b) + ",c=" + str(self.c)
+
+    @classmethod
+    def get_area_formula(cls):
+        return "(s×(s-a)×(s-b)×(s-c))^1/2"
+
+    @classmethod
+    def get_perimeter_formula(cls):
+        return "a+b+c"
 
 
 class EquilateralTriangle(Triangle):
-    pass
+    def __init__(self, a):
+        Triangle.__init__(self, a, b=a, c=a)
+
+    def __str__(self):
+        return "Equilateral Triangle,a = " + str(self.a)
 
 
 class Rectangle(Shape):
-    pass
+
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        if a < 0 or b < 0:
+            raise ValueError("Values of the parameters can't be below 0")
+
+    def get_area(self):
+        return self.a * self.b
+
+    def get_perimeter(self):
+        return 2 * self.a + 2 * self.b
+
+    def __str__(self):
+        return "Rectangle, a = " + str(self.a) + ", b = " + str(self.b)
+
+    @classmethod
+    def get_area_formula(cls):
+        return "a × b"
+
+    @classmethod
+    def get_perimeter_formula(cls):
+        return "2×a + 2×b"
 
 
 class Square(Rectangle):
-    pass
+
+
+    def __init__(self, a):
+        Rectangle.__init__(self, a, b = a)
+        if a < 0:
+            raise ValueError("Value of the parameter can't be below 0")
+
+    def __str__(self):
+        return "Square, a = " + str(self.a)
+
+    @classmethod
+    def get_area_formula(cls):
+        return "a × a"
+
+    @classmethod
+    def get_perimeter_formula(cls):
+        return "4×a"
 
 
 class RegularPentagon(Shape):
-    pass
+
+
+    def __init__(self, a):
+        self.a = a
+        if a < 0:
+            raise ValueError("Value of the parameter can't be below 0")
+
+    def get_area(self):
+        return (self.a ** 2) * (math.sqrt(5 * (5 + 2 * math.sqrt(5))) / 4)
+
+    def get_perimeter(self):
+        return (5 * self.a)
+
+    def __str__(self):
+        return "Regular Pentagon,a=" + str(self.a)
+
+    @classmethod
+    def get_area_formula(cls):
+        return "(a^2 × ((5×(5+2×(5)^1/2))^1/2)/4"
+
+    @classmethod
+    def get_perimeter_formula(cls):
+        return "5×a"
 
 
 class ShapeList:
-    pass
+
+
+    def __init__(self):
+        self.shapes = []
+
+    def add_shape(self, shape):
+
+        if not isinstance(shape, Shape):
+            raise TypeError("Error")
+        else:
+            self.shapes.append(shape)
+
+
+    def get_largest_shape_by_perimeter(self):
+
+        if len(self.shapes) > 0:
+            largest_shape = self.shapes[0] #
+            for shape in self.shapes:
+                if shape.get_perimeter() > largest_shape.get_perimeter():
+                    largest_shape = shape
+            return largest_shape
+        else:
+            return "You have no shapes added"
+
+
+
+    def get_largest_shape_by_area(self):
+
+        if len(self.shapes) > 0:
+            largest_shape = self.shapes[0]
+            for shape in self.shapes:
+                if shape.get_area() > largest_shape.get_area():
+                    largest_shape = shape
+            return largest_shape
+        else:
+            return "You have no shapes added"
+
+    def get_shapes_table(self):
+        table = []
+        title_list = ['idx', 'Class', '__str__ ', 'Perimeter', 'Formula', 'Area', 'Formula']
+        for i in range(0, len(self.shapes)):
+            table.append([str(i), str(self.shapes[i].__class__.__name__), str(self.shapes[i]),
+                          str(round(self.shapes[i].get_perimeter(), 2)), str(self.shapes[i].get_perimeter_formula()),
+                          str(round(self.shapes[i].get_area(), 2)), str(self.shapes[i].get_area_formula())])
+    def get_shapes_table(self):
+        table = []
+        title_list = ['idx', 'Class', '__str__ ', 'Perimeter', 'Formula', 'Area', 'Formula']
+        for i in range(0, len(self.shapes)):
+            table.append([str(i), str(self.shapes[i].__class__.__name__), str(self.shapes[i]),
+                          str(round(self.shapes[i].get_perimeter(), 2)), str(self.shapes[i].get_perimeter_formula()),
+                          str(round(self.shapes[i].get_area(), 2)), str(self.shapes[i].get_area_formula())])
+        return str(ui.print_table(table, title_list))  # table implemented in ui module
--- main.py
+++ main.py
@@ -1,28 +1,96 @@
 import sys
+import os
 from geometry import *
-
+import ui
 
 def main():
-
+    menu_options = ["Add new shape", "Show all shapes", "Show shape with the largest perimeter",
+    "Show shape with the largest area", "Show formulas"]
     shapes = ShapeList()  # object containing all shapes added by the user
     while True:
-        # TODO: implement user interaction here. You can change the code below
-        option = input("Select an option: ")
+        ui.menu("Geometry Learning",menu_options)
+        option = input("\nSelect an option: ")
+        ui.cls()
         if option == "1":
-            # Add new shape
-            pass
+            list_shapes = ["Circle", "Triangle", "Equilateral Triangle", "Rectangle", "Square", "Regular Pentagon"]
+            while True:
+                ui.menu("Choose a shape", list_shapes)
+                choosen_shape = input(">")
+                if choosen_shape == "1":
+                    sh = Circle(ui.multi_input("Input r = ")[0])
+                    shapes.add_shape(sh)
+                elif choosen_shape == '2':
+                        ui.cls()
+                        values = ui.multi_input("Input a =  b =  c =  ")
+                        sh = Triangle(values[0], values[1], values[2])
+                        print(sh)
+                        shapes.add_shape(sh)
+                elif choosen_shape == '3':
+                        ui.cls()
+                        sh = EquilateralTriangle(ui.multi_input("Input a = ")[0])
+                        shapes.add_shape(sh)
+                elif choosen_shape == '4':
+                        ui.cls()
+                        values = ui.multi_input("Input a = b =")
+                        sh = Rectangle(values[0], values[1])
+                        shapes.add_shape(sh)
+                elif choosen_shape == '5':
+                        ui.cls()
+                        sh = Square(ui.multi_input("Input a = ")[0])
+                        shapes.add_shape(sh)
+                elif choosen_shape == '6':
+                        ui.cls()
+                        sh = Circle(ui.multi_input("Input a = ")[0])
+                        shapes.add_shape(sh)
+                elif choosen_shape == '0':
+                        break
         elif option == "2":
             # Show all shapes
-            pass
+            ui.cls()
+            shapes.get_shapes_table()
+
         elif option == "3":
             # Show shape with the largest perimeter
-            pass
+            ui.cls()
+            largest_perimeter = shapes.get_largest_shape_by_perimeter()
+            print("Shape that have largest perimeter: {}".format(largest_perimeter))
         elif option == "4":
             # Show shape with the largest area
-            pass
+            ui.cls()
+            largest_area = shapes.get_largest_shape_by_area()
+            print("Shape that have largest area: {}".format(largest_area))
         elif option == "5":
-            # Show formulas
-            pass
+            ui.cls()
+            print("Which shape formulas do you want to see?")
+            options = ["Circle", "Triangle", "Equilateral Triangle", "Rectangle", "Square", "Regular Pentagon"]
+            ui.menu("Choose option:", options)
+            option = input("> ")
+            if option == '1':
+                ui.cls()
+                print("perimeter = " + Circle.get_perimeter_formula() + "\narea = " +
+                Circle.get_area_formula())
+            elif option == '2':
+                ui.cls()
+                print("perimeter = " + Triangle.get_perimeter_formula() + "\narea = " +
+                Triangle.get_area_formula())
+            elif option == '3':
+                ui.cls()
+                print("perimeter = " + EquilateralTriangle.get_perimeter_formula() + "\narea = " +
+                EquilateralTriangle.get_area_formula())
+            elif option == '4':
+                ui.cls()
+                print("perimeter = " + Rectangle.get_perimeter_formula() + "\narea = " +
+                Rectangle.get_area_formula())
+            elif option == '5':
+                ui.cls()
+                print("perimeter = " + Square.get_perimeter_formula() + "\narea = " +
+                Square.get_area_formula())
+            elif option == '6':
+                ui.cls()
+                print("perimeter = " + RegularPentagon.get_perimeter_formula() + "\narea = " +
+                RegularPentagon.get_area_formula())
+            elif option == '0':
+                break
         elif option == "0":
             sys.exit()
 
